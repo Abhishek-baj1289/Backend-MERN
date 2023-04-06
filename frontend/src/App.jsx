99ce4f8c319efafe00929dc3d8feeda4326b4438
VeloTeam-App <abhishek.bajpai@velocitai.com>
2023-04-06 18:15:51 +0530

    did basic frontend fetching

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [data, setData] = useState([]);
+  useEffect(()=>{
+    const fetchData = async()=>{
+      const response = await fetch('http://localhost:3500/users');
+      const responseData = await response.json()
+      setData(responseData)
+    };
+    fetchData()
+  },[])
+
+  return (
+    <div className="App">
+      <h3>All users:</h3>
+      <ul>
+        {data.map(item=>(
+          <li key={item._id}>Name: {item.firstname} {item.lastname} <br/>Status: {item.status}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
